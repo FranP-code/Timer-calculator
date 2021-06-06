73022c9fb3d11cf6503883a875d7bdf4a5e0ad7c
Francisco P <franpessano1@gmail.com>
2021-06-06 00:05:05 -0300

    CSS de la seccion 2 listo

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let userAgentData = navigator.userAgent
+let button = document.getElementById('button')
+let buttonInfo = document.getElementById('button-info')
 
 console.log(userAgentData)
 
 if(userAgentData.indexOf("Firefox") > -1) {
-    document.getElementById('button').className = "fix-button-for-firefox";
+    button.className = "fix-button-for-firefox";
 }
+
+//* Credits: https://www.geeksforgeeks.org/how-to-detect-the-user-browser-safari-chrome-ie-firefox-and-opera-using-javascript/ && https://developer.mozilla.org/en-US/docs/Web/API/NavigatorID/userAgent
+
+button.addEventListener('mouseenter', () => buttonInfo.style.opacity = '100%')
+button.addEventListener('mouseleave', () => buttonInfo.style.opacity = '0%')
+
+//* Credits: https://stackoverflow.com/questions/52274130/css-transition-on-javascript-changing-style
